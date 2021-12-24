9314d86f9fa597b455a0dd367017dbd52f0fbc99
diMASasin <35368058+diMASasin@users.noreply.github.com>
2021-12-24 20:11:50 +0500

    Update list.cpp

--- 2list-5/list.cpp
+++ 2list-5/list.cpp
@@ -1,302 +1,217 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include"subj.h"                        
+#include <iostream>
+#include "list.h"
 
-class Auto : public Base
-{
-private:
-	char brand[MAX_STRING_LENGTH];
-	char color[MAX_STRING_LENGTH];
+using namespace std;
 
-public:
-	void Print()
+const string TypeName[4]= 
+            { "(unknown)", "Auto", "Aircraft", "Ship" };
+
+
+	Node::Node()
 	{
-		Base::BasePrint();
-		printf("Бренд: %s\n", brand);
-		printf("Цвет: %s\n\n", color);
+		prev = NULL;
+		next = NULL;
+		list = NULL;
+	}
+	
+	Node::~Node()
+	{
+		if(list)
+			list->Remove(this);
 	}
 
-	void Input()
+	List::List()
 	{
-		Base::BaseInput();
-		printf("Бренд: ");
-		scanf("%s", brand);
-		printf("Цвет: ");
-		scanf("%s", color);
+		head = NULL;
+		tail = NULL;
 	}
-};
-
-class Aircraft : public Base
-{
-private:
-	int numberOfCrewMembers;
-	int numberOfEngines;
-	double maxHeight;
-
-public:
-	void Print()
+	
+	List::~List()
 	{
-		Base::BasePrint();
-		printf("Количество членов экипажа: %d\n", numberOfCrewMembers);
-		printf("Количество двигателей: %d\n", numberOfEngines);
-		printf("Максимальная высота: %lg\n\n", maxHeight);
+		Clear();
 	}
-
-	void Input()
+	
+	Node* Node::getNext() const
 	{
-		Base::BaseInput();
-		printf("Количество членов экипажа: ");
-		scanf("%d", &numberOfCrewMembers);
-		printf("Количество двигателей: ");
-		scanf("%d", &numberOfEngines);
-		printf("Максимальная высота: ");
-		scanf("%lg", &maxHeight);
+		return next;
 	}
-};
-
-class Ship : public Base
-{
-private:
-	int numberOfCrewMembers;
-	int numberOfEngines;
-	char name[MAX_STRING_LENGTH];
-	char homePort[MAX_STRING_LENGTH];
-
-public:
-	void Print()
+	
+	Node* Node::getPrev() const
 	{
-		Base::BasePrint();
-		printf("Количество членов экипажа: %d\n", numberOfCrewMembers);
-		printf("Количество двигателей: %d\n", numberOfEngines);
-		printf("Название: %s\n", name);
-		printf("Порт приписки: %s\n\n", homePort);
+		return prev;
+	}
+	
+	Node* List::getHead() const
+	{
+		return head;
+	}
+	
+	Node* List::getTail() const
+	{
+		return tail;
+	}
+	
+	List* Node::IsInList() const
+	{
+		return list;
 	}
 
-	void Input()
-	{
-		Base::BaseInput();
-		printf("Количество членов экипажа: ");
-		scanf("%d", &numberOfCrewMembers);
-		printf("Количество двигателей: ");
-		scanf("%d", &numberOfEngines);
-		printf("Название: ");
-		scanf("%s", name);
-		printf("Порт приписки: ");
-		scanf("%s", homePort);
-	}
-};
-
-void Base::BasePrint()
-{
-	printf("Модель: %s\n", model);
-	printf("Расход топлива: %lg\n", fuelConsumption);
-	printf("Максимальная скорость: %lg\n", maxSpeed);
-	printf("Максимальная дальность движения: %lg\n", rangeOfMovement);
-	printf("Количество мест: %d\n", maxPassengerNumber);
-	printf("Максимальный вес багажа: %lg\n", maxBaggageWeight);
-}
-
-void Base::Print()
-{
-	printf("%s:\n", TypeName[(this->type > 0 && this->type <= 3) ? this->type : 0]);
-	switch (this->type)
-	{
-	case itAuto:
-		((Auto*)this)->Print();
-		break;
-	case itAircraft:
-		((Aircraft*)this)->Print();
-		break;
-	case itShip:
-		((Ship*)this)->Print();
-		break;
-	}
-}
-
-Base* Base::Create(ItemType type, List* list)
-{
-	if (!list)
-		return NULL;
-
-	Base* p;
-	switch (type)
-	{
-	case itAuto:
-		p = new Auto();
-		break;
-	case itAircraft:
-		p = new Aircraft();
-		break;
-	case itShip:
-		p = new Ship();
-		break;
-	default:
-		printf("Ошибка!\n");
-		return NULL;
-	}
-
-	p->type = type;
-	return p;
-}
-
-void Base::Input()
-{
-	if (!this)
-		return;
-
-	switch (this->type)
-	{
-	case itAuto:
-		((Auto*)this)->Input();
-		break;
-	case itAircraft:
-		((Aircraft*)this)->Input();
-		break;
-	case itShip:
-		((Ship*)this)->Input();
-		break;
-	default:
-		printf("Ошибка!\n");
-		break;
-	}
-}
-
-void Base::BaseInput()
-{
-	printf("Модель: ");
-	scanf("%s", model);
-	printf("Расход топлива: ");
-	scanf("%lg", &fuelConsumption);
-	printf("Максимальная скорость: ");
-	scanf("%lg", &maxSpeed);
-	printf("Максимальная дальность движения: ");
-	scanf("%lg", &rangeOfMovement);
-	printf("Количество мест: ");
-	scanf("%d", &maxPassengerNumber);
-	printf("Максимальный вес багажа: ");
-	scanf("%lg", &maxBaggageWeight);
-}
-
-void SubjList::PrintTransportList()
-{
-	Node* p;
-	int i = 0;
-	for (p = getHead(); p; p = p->getNext())
-	{
-		printf("%d. ", i);
-		((Base*)p)->Print();
-		i++;
-	}
-	printf("\n");
-}
-
-void SubjList::SortByRangeOfMovement()
-{
-	if (!getHead())
-	{
-		printf("Список пуст.\n\n");
-		return;
-	}
-
-	Base* i, * j;
-	Node* last;
-	last = getTail();
-
-	for (i = (Base*)getHead(); i->getNext(); i = (Base*)i->getNext())
-	{
-		for (j = (Base*)getHead(); j->getNext() && (Node*)j != last; j = (Base*)j->getNext())
+    void List::Add(Node* element)
+    {
+    	if(!element || element->list)
+    		return;
+    		
+    	element->list = this;
+    	
+        if (head == NULL && tail == NULL) 
 		{
-			if (j->rangeOfMovement > ((Base*)j->getNext())->rangeOfMovement)
-			{
-				Node* temp;
-				temp = j->getNext();
-				Remove(j->getNext());
-				Insert(temp, GetIndex((Node*)j));
-				if (temp == last)
-					last = (Node*)j;
-				if (j == i)
-					i = (Base*)temp;
-				else if (temp == (Node*)i)
-					i = j;
-				j = (Base*)temp;
-			}
-		}
-		last = last->getPrev();
-	}
-}
+            head = element;
+            tail = element;
+            return;
+        }
 
-void SubjList::SortByMaxSpeed()
-{
-	if (!getHead())
-	{
-		printf("Список пуст.\n\n");
-		return;
-	}
+        tail->next = element;
+        element->prev = tail;
+        tail = element;
+        element->next = NULL;
+    }
 
-	Base* i, * j;
-	Node* last;
-	last = getTail();
+    void List::Insert(Node* newElement, int index)
+    {
+    	if(!newElement || index < 0 || newElement->list)
+    		return;
+    	
+    	
+        Node* item;
+        item = GetItem(index);
 
-	for (i = (Base*)getHead(); i->getNext(); i = (Base*)i->getNext())
-	{
-		for (j = (Base*)getHead(); j->getNext() && (Node*)j != last; j = (Base*)j->getNext())
+        if (item) 
 		{
-			if (j->maxSpeed > ((Base*)j->getNext())->maxSpeed)
-			{
-				Node* temp;
-				temp = j->getNext();
-				Remove(j->getNext());
-				Insert(temp, GetIndex((Node*)j));
-				if (temp == last)
-					last = (Node*)j;
-				if (j == i)
-					i = (Base*)temp;
-				else if (temp == (Node*)i)
-					i = j;
-				j = (Base*)temp;
-			}
+            if (item->prev != NULL)
+                item->prev->next = newElement;
+            else
+                head = newElement;
+                
+            newElement->next = item;
+            newElement->prev = item->prev;
+            item->prev = newElement;
+    		newElement->list = this;
+        }
+        else 
+		{
+            Add(newElement);
+        }
+    }
+
+    int List::Count()
+    {
+		Node* currentNode;
+        int count = 0;
+        currentNode = head;
+		
+		if(!currentNode)
+			return count;
+		
+        while (currentNode) 
+		{
+            count++;
+            currentNode = currentNode->next;
+        }
+
+        return count;
+    }
+
+    Node* List::GetItem(int index)
+    {
+    	if(index < 0)
+    		return NULL;
+    	
+        Node* item;
+        int i;
+
+        item = head;
+
+        for (i = 0; i < index; i++)
+        {
+        	if(!item)
+        		return NULL;
+        	
+        	item = item->next;
 		}
-		last = last->getPrev();
-	}
-}
 
-void SubjList::FindByMaxPassengerNumber()
-{
-	if (!getHead())
-	{
-		printf("Список пуст.\n\n");
-		return;
-	}
+        return item;
+    }
 
-	printf("Введите искомое число мест: ");
-	int num;
-	scanf("%d", &num);
+    Node* List::Remove(int index)
+    {
+    	if(index < 0)
+    		return NULL;
+    		
+        Node* item;
+        item = GetItem(index);
 
-	Base* i;
-	for (i = (Base*)getHead(); i; i = (Base*)i->getNext())
-	{
-		if (i->maxPassengerNumber == num)
-			i->Print();
-	}
-}
+		Remove(item);
+        return item;
+    }
+    
+    Node* List::Remove(Node* item)
+    {
+        if (!item || item->list != this)
+            return NULL;
 
-void SubjList::FindByMaxWight()
-{
-	if (!getHead())
-	{
-		printf("Список пуст.\n\n");
-		return;
-	}
+        if (item->prev != NULL)
+            item->prev->next = item->next;
+        if (item->next != NULL)
+            item->next->prev = item->prev;
 
-	printf("Введите искомый максимально допустимый вес: ");
-	int num;
-	scanf("%d", &num);
+        if (item == head)
+            head = item->next;
+        if (item == tail)
+            tail = item->prev;
+            
+        item->next = NULL;
+        item->prev = NULL;
+        item->list = NULL;
 
-	Base* i;
-	for (i = (Base*)getHead(); i; i = (Base*)i->getNext())
-	{
-		if (i->maxBaggageWeight == num)
-			i->Print();
-	}
-}
+        return item;
+    }
+
+    int List::Delete(int index)
+    {
+    	if(index < 0)
+    		return 0;
+    		
+        Node* item;
+
+        item = Remove(index);
+        if (!item)
+            return 0;
+
+        delete item;
+        return 1;
+    }
+
+    int List::GetIndex(Node* element)
+    {
+    	if(!element)
+    		return -1;
+    		
+        Node* item;
+        item = head;
+        int i;
+
+        for (i = 0; item != NULL; i++) 
+		{
+            if (item == element)
+                return i;
+
+            item = item->next;
+        }
+        return -1;
+    }
+
+    void List::Clear()
+    {
+        while (head)
+            Delete(0);
+    }
